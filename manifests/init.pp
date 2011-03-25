cfd9b4317bdcc3a77d94474c9fbcd887c8d003a0
drebs <drebs@riseup.net>
2011-03-25 17:28:10 -0300

    adding usbutils dependency

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,7 @@
 class ekeyd {
 
+  package { "usbutils": ensure => installed }
+ 
   if $ekeyd_key_present != 'true' { fail("Can't find an ekey key plugged into usb on ${fqdn}") }
   if !$ekey_masterkey { fail("You need to define \$ekey_masterkey for ${fqdn}") }
 
