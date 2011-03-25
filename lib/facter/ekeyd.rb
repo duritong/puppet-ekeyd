f11ea39e640c2022f36d97e7965cf4b8af6d70b2
drebs <drebs@riseup.net>
2011-03-25 17:23:17 -0300

    fixing exec

--- lib/facter/ekeyd.rb
+++ lib/facter/ekeyd.rb
@@ -1,6 +1,6 @@
 Facter.add('ekeyd_key_present') do
   confine :kernel => %w{Linux}
   setcode do
-    !Kernel.exec('lsusb | grep "Entropy Key"').empty?
+    !`lsusb | grep "Entropy Key`.empty?
   end
 end
