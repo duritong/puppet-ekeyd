d64575a7c51188e99585920b19b0c85b58565f28
mh <mh@immerda.ch>
2012-10-02 01:02:12 +0200

    we can't concatenate variables in the same class

--- manifests/monit.pp
+++ manifests/monit.pp
@@ -1,15 +1,13 @@
 class ekeyd::monit {
-  $customlines = [ 'if 5 restarts within 5 cycles then timeout' ]
-  
-  if $ekeyd::host {
-    $customlines += [ 'if failed host 127.0.0.1 port 8888 then restart' ]
-  }
-  
   monit::check::process{"ekeyd":
     pidfile     => "/var/run/ekeyd.pid",
     start       => "/sbin/service ekeyd start",
     stop        => "/sbin/service ekeyd stop",
-    customlines => $customlines,
+    customlines => $ekeyd::host ? {
+      false     => [ 'if 5 restarts within 5 cycles then timeout' ],
+      default   => [ 'if failed host 127.0.0.1 port 8888 then restart',
+                     'if 5 restarts within 5 cycles then timeout' ]
+    },
     require => Service['ekeyd'],
   }
 }
