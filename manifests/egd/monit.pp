eb0a1fd119d6d4f7a2f2138bfc15217a65cf441a
mh <mh@immerda.ch>
2012-10-02 01:36:24 +0200

    debian has the service cmd somewhere else

--- manifests/egd/monit.pp
+++ manifests/egd/monit.pp
@@ -3,10 +3,14 @@ class ekeyd::egd::monit {
     debian  => 'ekeyd-egd-linux',
     default => 'egd-linux'
   }
+  $service_cmd = $::operatingsystem ? {
+    debian => '/usr/sbin/service',
+    default => '/sbin/service'
+  }
   monit::check::process{"egd-linux":
     pidfile     => "/var/run/${service_name}.pid",
-    start       => "/sbin/service ${service_name} start",
-    stop        => "/sbin/service ${service_name} stop",
+    start       => "${service_cmd}${service_name} start",
+    stop        => "${service_cmd} ${service_name} stop",
     customlines => [ 'if 5 restarts within 5 cycles then timeout' ],
     require => Service['egd-linux']
   }
--- manifests/monit.pp
+++ manifests/monit.pp
@@ -1,8 +1,12 @@
 class ekeyd::monit {
+  $service_cmd = $::operatingsystem ? {
+    debian => '/usr/sbin/service',
+    default => '/sbin/service'
+  }
   monit::check::process{"ekeyd":
     pidfile     => "/var/run/ekeyd.pid",
-    start       => "/sbin/service ekeyd start",
-    stop        => "/sbin/service ekeyd stop",
+    start       => "${service_cmd} ekeyd start",
+    stop        => "${service_cmd} ekeyd stop",
     customlines => $ekeyd::host ? {
       false     => [ 'if 5 restarts within 5 cycles then timeout' ],
       default   => [ 'if failed host 127.0.0.1 port 8888 then restart',
