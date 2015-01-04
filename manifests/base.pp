eaccf8d3c85cb12d83595d9101149538a3d38e7a
mh <mh@immerda.ch>
2015-01-05 00:53:25 +0100

    wee nedd to wait a little bit

--- manifests/base.pp
+++ manifests/base.pp
@@ -22,18 +22,18 @@ class ekeyd::base {
   service{'ekeyd':
     ensure => running,
     enable => true,
-  }
-
-  exec{'configure_ekeyd_key':
+  } ~> exec{'wait_for_ekeyd_to_be_started':
+    command     => 'bash -c "sleep 2"',
+    refreshonly => true,
+  } -> exec{'configure_ekeyd_key':
     command => "ekey-rekey \$(ekeydctl list | grep ',/dev' | awk -F, '{ print \$5 }') ${ekeyd::masterkey}",
     unless  => 'ekeydctl list | grep -q \'Running OK\'',
-    require => Service['ekeyd'],
   }
   sysctl::value{'kernel.random.write_wakeup_threshold':
     value => 1024
   }
   if $ekeyd::host {
-    Service['ekeyd']{
+    Exec['configure_ekeyd_key']{
       before => Service['egd-linux']
     }
   }
